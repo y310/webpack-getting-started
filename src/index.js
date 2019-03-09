309da847bcb63518f8e00b0905f74733a5deafcf
Yusuke Mito <y310.1984@gmail.com>
2019-03-09 16:33:17 +0900

    Done Hot Module Replacement

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import printMe from './print.js';
+import './style.css';
 
 function component() {
   let element = document.createElement('div');
@@ -15,4 +16,14 @@ function component() {
   return element;
 }
 
-document.body.appendChild(component());
+let element = component();
+document.body.appendChild(element);
+
+if(module.hot) {
+  module.hot.accept('./print.js', function() {
+    console.log('Accepting the updated printMe module!');
+    document.body.removeChild(element);
+    element = component();
+    document.body.appendChild(element);
+  });
+}
