a4ae61ad8f0fa5e98c000a1f5ad2d94c1793a7be
Yusuke Mito <y310.1984@gmail.com>
2019-03-09 14:17:40 +0900

    Done Asset Management

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import _ from 'lodash';
+import './style.css';
+import Photo from './photo.jpg';
+import Data from './data.xml';
+import Csv from './data.csv';
 
 function component() {
   let element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  var photo = new Image();
+  photo.src = Photo;
+
+  element.appendChild(photo);
+
+  console.log(Data, Csv);
 
   return element;
 }
